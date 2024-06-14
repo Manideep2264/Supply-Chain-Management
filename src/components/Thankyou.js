4fbd54b18a6119193da6bd9e2ed98b0b30e8a864
Manideep2264 <manideep2264@gmail.com>
2024-06-14 15:51:49 +0530

    first commit

---
+++ src/components/Thankyou.js
@@ -0,0 +1,10 @@
+function Thankyou(){
+    return(
+        <div>
+           <h1 style={{color:"white",marginLeft:"800px"}}>Booking Confirmed!!!!!</h1>
+           <h1 style={{color:"white",marginLeft:"800px"}}>Thankyou.....</h1>
+        </div>
+        
+    );
+}
+export default Thankyou;
